b16771de9812a746c0c2a7a55e1b1a366fd0bca1
kamiljot <kamiljotzia@gmail.com>
2025-07-01 17:43:39 +0200

    Code cleanup

--- src/kernels/add/add.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-extern "C" float run_add_global(const float* a, const float* b, float* c, int N);
-extern "C" float run_add_shared(const float* a, const float* b, float* c, int N);
-extern "C" float run_add_float4(const float* a, const float* b, float* c, int N);
